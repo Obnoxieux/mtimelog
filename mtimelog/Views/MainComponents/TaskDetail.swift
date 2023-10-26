7124ebb68346fd0881f640af8fa44e20a079144e
David Battefeld <davidbattefeld@gmail.com>
2023-10-26 16:48:22 +0200

    feat: add delete action to detail view

--- mtimelog/Views/MainComponents/TaskDetail.swift
+++ mtimelog/Views/MainComponents/TaskDetail.swift
@@ -10,82 +10,113 @@ import SwiftUI
 struct TaskDetail: View {
     @AppStorage("hoursInWorkingDay") var hoursInWorkingDay = 8
     @State var showFinishSheet = false
+    @State var showingDeleteConfirmationDialog = false
+    @Environment(\.modelContext) private var modelContext
     
-    var task: Task
+    @State private var taskDeleted = false
+    
+    @Bindable var task: Task
     
     let listPadding: CGFloat = 7
     
     var body: some View {
-        
-        List {
-            let gaugePercentage = task.calculatePercentageOfWorkingDay(hoursInWorkingDay: hoursInWorkingDay)
-            switch task.status {
-                
-            case .ongoing:
-                TaskDetailMainInfo(task: task)
-                    .padding(.vertical, listPadding)
-                VStack(spacing: 10) {
+        if taskDeleted == false {
+            List {
+                let gaugePercentage = task.calculatePercentageOfWorkingDay(hoursInWorkingDay: hoursInWorkingDay)
+                switch task.status {
+                    
+                case .ongoing:
+                    TaskDetailMainInfo(task: task)
+                        .padding(.vertical, listPadding)
+                    VStack(spacing: 10) {
+                        HStack {
+                            Text("Started: ") + Text(task.startTime, style: .time).font(.headline)
+                            Spacer()
+                            Text(task.startTime, style: .timer)
+                                .font(.headline)
+                        }
+                        .padding(.vertical, listPadding)
+                        
+                        
+                        Gauge(value: gaugePercentage) {
+                            Text("% of Workday")
+                        }
+                        .padding(.vertical, listPadding)
+                    }
+                    Label(task.status.rawValue, systemImage: "info.square")
+                        .foregroundColor(task.status.color)
+                        .padding(.vertical, listPadding)
                     HStack {
-                        Text("Started: ") + Text(task.startTime, style: .time).font(.headline)
                         Spacer()
-                        Text(task.startTime, style: .timer)
-                            .font(.headline)
+                        Button("Finish Task") {
+                            showFinishSheet = true
+                        }
+                        .buttonStyle(.borderedProminent)
+                        Spacer()
                     }
                     .padding(.vertical, listPadding)
                     
-                   
-                    Gauge(value: gaugePercentage) {
-                        Text("% of Workday")
-                    }
-                    .padding(.vertical, listPadding)
-                }
-                Label(task.status.rawValue, systemImage: "info.square")
-                    .foregroundColor(task.status.color)
-                    .padding(.vertical, listPadding)
-                HStack {
-                    Spacer()
-                    Button("Finish Task") {
-                        showFinishSheet = true
-                    }
-                    .buttonStyle(.borderedProminent)
-                    Spacer()
-                }
-                .padding(.vertical, listPadding)
-                
-            default:
-                TaskDetailMainInfo(task: task)
-                    .padding(.vertical, listPadding)
-                if let endTime = task.endTime {
-                    HStack {
-                        Image(systemName: "clock")
-                            .foregroundColor(.accentColor)
-                        Text(task.startTime...endTime)
-                    }
-                    .padding(.vertical, listPadding)
-                    Label(task.getDuration(), systemImage: "hourglass")
+                default:
+                    TaskDetailMainInfo(task: task)
                         .padding(.vertical, listPadding)
-                    Gauge(value: gaugePercentage) {
-                        Text("% of Workday")
+                    if let endTime = task.endTime {
+                        HStack {
+                            Image(systemName: "clock")
+                                .foregroundColor(.accentColor)
+                            Text(task.startTime...endTime)
+                        }
+                        .padding(.vertical, listPadding)
+                        Label(task.getDuration(), systemImage: "hourglass")
+                            .padding(.vertical, listPadding)
+                        Gauge(value: gaugePercentage) {
+                            Text("% of Workday")
+                        }
+                        .padding(.vertical, listPadding)
                     }
-                    .padding(.vertical, listPadding)
-                }
-                if let comment = task.statusComment {
-                    Label(comment, systemImage: task.status == .blocked ? "exclamationmark.bubble" : "bubble.left")
+                    if let comment = task.statusComment {
+                        Label(comment, systemImage: task.status == .blocked ? "exclamationmark.bubble" : "bubble.left")
+                            .padding(.vertical, listPadding)
+                    }
+                    Label(task.status.rawValue, systemImage: "info.square")
+                        .foregroundColor(task.status.color)
                         .padding(.vertical, listPadding)
                 }
-                Label(task.status.rawValue, systemImage: "info.square")
-                    .foregroundColor(task.status.color)
-                    .padding(.vertical, listPadding)
             }
-        }
-        .sheet(isPresented: $showFinishSheet) {
-            FinishTaskSheet(task: task)
+            .sheet(isPresented: $showFinishSheet) {
+                FinishTaskSheet(task: task)
+            }
+            .confirmationDialog("Confirm Deletion", isPresented: $showingDeleteConfirmationDialog) {
+                Button("Delete Task", role: .destructive) {
+                    taskDeleted = true
+                    modelContext.delete(task)
+                }
+                Button("Cancel", role: .cancel) {}
+            } message: {
+                Text("This task will be deleted from its corresponding working day.")
+            }
+            .toolbar {
+                ToolbarItemGroup {
+                    Spacer()
+                    Button {
+                        // TODO: edit
+                    } label: {
+                        Image(systemName: "pencil.and.list.clipboard")
+                    }
+                    Button {
+                        showingDeleteConfirmationDialog = true
+                    } label: {
+                        Image(systemName: "trash")
+                    }
+                    Spacer()
+                    ShareLink(item: "bogus")
+                }
+            }
+        } else {
+            ContentUnavailableView("No Task selected", systemImage: "note.text")
         }
     }
 }
 
-struct TaskDetail_Previews: PreviewProvider {
-    static var previews: some View {
-        TaskDetail(task: Task.sampleData[0])
-    }
+#Preview {
+    TaskDetail(task: Task.sampleData[0])
 }
