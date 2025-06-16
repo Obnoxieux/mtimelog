161785c9e4c2e0aba436e7e3dd6e226b91203763
David Battefeld <davidbattefeld@gmail.com>
2025-06-16 23:14:31 +0200

    build: use current macOS SDK

--- mtimelog/Views/Task/Detail/TaskDetailMainInfo.swift
+++ mtimelog/Views/Task/Detail/TaskDetailMainInfo.swift
@@ -15,7 +15,6 @@ struct TaskDetailMainInfo: View {
             VStack(alignment: .leading) {
                 Label(task.projectID, systemImage: "tray.full")
                     .bold()
-                // .labelStyle(.titleOnly)
                 Label(task.taskDescription ?? "No description provided", systemImage: "list.bullet.clipboard")
             }
             Spacer()
