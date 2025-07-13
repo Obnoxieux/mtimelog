e87aa9836a25193d383ebb0d075b12f2149aa58e
David Battefeld <davidbattefeld@gmail.com>
2025-07-13 13:41:01 +0200

    feat: add report email settings

---
+++ mtimelog/Model/ReportFormat.swift
@@ -0,0 +1,13 @@
+//
+//  ReportFormat.swift
+//  mtimelog
+//
+//  Created by David Battefeld on 13.07.25.
+//
+
+import Foundation
+
+enum ReportEmailFormat: String, Codable {
+    case plainText
+    case html
+}
--- mtimelog/Views/Settings/GeneralSettingsTab.swift
+++ mtimelog/Views/Settings/GeneralSettingsTab.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct GeneralSettingsTab: View {
     @AppStorage("hoursInWorkingDay") var hoursInWorkingDay = 8
     @AppStorage("showTimesInReport") var showTimesInReport = true
-    
+    @AppStorage("reportEmailRecipient") var reportEmailRecipient = ""
+    @AppStorage("reportEmailFormat") var reportEmailFormat = ReportEmailFormat
+        .plainText
+
     var body: some View {
         Form {
-            TextField("Hours per Working Day", value: $hoursInWorkingDay, format: .number)
+            TextField(
+                "Hours per Working Day",
+                value: $hoursInWorkingDay,
+                format: .number
+            )
+            .textFieldStyle(.roundedBorder)
+
+            Toggle(
+                "Include task times and duration in report",
+                isOn: $showTimesInReport
+            )
+
+            Picker("Report Email Format", selection: $reportEmailFormat) {
+                Text("Plain Text").tag(ReportEmailFormat.plainText)
+                Text("HTML").tag(ReportEmailFormat.html)
+            }
+            .pickerStyle(.inline)
+
+            TextField("Report Email Recipient", text: $reportEmailRecipient)
+                .textContentType(.emailAddress)
                 .textFieldStyle(.roundedBorder)
-            Toggle("Include task times and duration in report", isOn: $showTimesInReport)
         }
         .textSelection(.enabled)
         .formStyle(.grouped)
